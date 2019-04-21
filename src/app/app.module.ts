f0be82f7425e5b55b21dc8da9d249ebf196ab0ef
Ksu <k.pyagay@unios.io>
2019-04-22 00:04:55 +0300

    new services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {MatButtonModule, MatCheckboxModule, MatSidenavModule} from '@angular/material';
+import {MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatInputModule, MatSidenavModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
-import { ToolbarComponent } from './main-page/toolbar.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 import { DialogListPageComponent } from './dialog-list-page/dialog-list-page.component';
 import {MatIconModule} from '@angular/material/icon';
 import { DialogCardComponent } from './dialog-card/dialog-card.component';
@@ -12,6 +12,14 @@ import { SidenavComponent } from './sidenav/sidenav.component';
 import {DialogPageComponent} from './dialog-page/dialog-page.component';
 import { PlaylistPageComponent } from './playlist-page/playlist-page.component';
 import {MatMenuModule} from '@angular/material';
+import { MainComponent } from './pages/main/main.component';
+import {RouterModule} from '@angular/router';
+import {AppRoutingModule} from './app-routing.module';
+import { AuthComponent } from './pages/auth/auth.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { LoginFormComponent } from './auth/login-form/login-form.component';
+import {AuthModule} from './auth/auth.module';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -21,17 +29,26 @@ import {MatMenuModule} from '@angular/material';
     DialogCardComponent,
     SidenavComponent,
     DialogPageComponent,
-    PlaylistPageComponent
+    PlaylistPageComponent,
+    MainComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     MatButtonModule,
     MatCheckboxModule,
     MatIconModule,
     NoopAnimationsModule,
     BrowserAnimationsModule,
     MatSidenavModule,
-    MatMenuModule
+    MatMenuModule,
+    AppRoutingModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
