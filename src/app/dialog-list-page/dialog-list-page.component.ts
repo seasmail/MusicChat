5179c909719fd8365c52a88c2874ff5089953f90
Ksu <k.pyagay@unios.io>
2019-04-25 21:32:04 +0300

    hover and active li

--- src/app/dialog-list-page/dialog-list-page.component.ts
+++ src/app/dialog-list-page/dialog-list-page.component.ts
@@ -9,18 +9,22 @@ import {Chat} from '../models/chat';
 })
 export class DialogListPageComponent implements OnInit {
   @Input() chats: Chat[];
-
-
+  public selectedChat: Chat;
   constructor(
     private chatService: ChatService) { }
 
   ngOnInit() {
     console.log('LIST ON INIT');
+    this.chatService.currentChat.subscribe(chat => this.selectedChat = chat);
   }
 
   onSelect(chat: Chat) {
     this.chatService.changeChat(chat);
+    this.selectedChat = chat;
+    console.log('selected chat ' + this.selectedChat);
     console.log('list ' + JSON.stringify(this.chats));
   }
+
+
 
 }
